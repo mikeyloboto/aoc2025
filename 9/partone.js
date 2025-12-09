27be26be3f8ef9d8ce90ccfab13614c5c9ee9e4d
Evilbas <dimonloboto@gmail.com>
2025-12-09 13:11:04 +0800

    Day 9 part 1

--- 9/partone.js
+++ 9/partone.js
@@ -1,7 +1,29 @@
 const fs = require("fs");
 
-fs.readFile("./testinput", "utf8", (err, data) => {
+fs.readFile("./input", "utf8", (err, data) => {
   if (err) return;
-  const cleanData = data.trim().split("\n");
-  console.log(cleanData);
+  const cleanData = data
+    .trim()
+    .split("\n")
+    .map((row) => row.split(",").map(Number));
+  const maxSize = cleanData.reduce(
+    (a, c) => {
+      return [Math.max(a[0], c[0]), Math.max(a[1], c[1])];
+    },
+    [0, 0],
+  );
+
+  let maxRect = 0;
+
+  for (let i = 0; i < cleanData.length - 1; i++) {
+    for (let j = i + 1; j < cleanData.length; j++) {
+      const size =
+        (Math.abs(cleanData[i][0] - cleanData[j][0]) + 1) *
+        (Math.abs(cleanData[i][1] - cleanData[j][1]) + 1);
+      if (size > maxRect) {
+        maxRect = size;
+      }
+    }
+  }
+  console.log(maxRect);
 });
